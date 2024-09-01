4a3a070ab940e180efe421ce293b99ae6b277e25
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2024-09-01 01:25:05 -0700

    Imported Folder System Packages
    Decided to import these packages too since they also support the project visually

---
+++ Assets/Gaskellgames/Folder System/Resources/Scripts/HierarchyFolders.cs
@@ -0,0 +1,65 @@
+﻿#if UNITY_EDITOR
+using UnityEngine;
+
+/// <summary>
+/// Code updated by Gaskellgames
+/// </summary>
+
+namespace Gaskellgames.FolderSystem
+{
+    [ExecuteInEditMode]
+    public class HierarchyFolders : GGMonoBehaviour
+    {
+        #region Variables
+        
+        public enum TextAlignment { Left, Center }
+        
+        [SerializeField]
+        public bool customText;
+        
+        [SerializeField]
+        public bool customIcon;
+        
+        [SerializeField]
+        public bool customHighlight;
+        
+        [SerializeField]
+        public Color32 textColor = InspectorUtility.textNormalColor;
+        
+        [SerializeField]
+        public Color32 iconColor = InspectorUtility.textNormalColor;
+        
+        [SerializeField]
+        public Color32 highlightColor = InspectorUtility.cyanColor;
+        
+        [SerializeField]
+        public FontStyle textStyle = FontStyle.BoldAndItalic;
+        
+        [SerializeField]
+        public TextAlignment textAlignment = TextAlignment.Left;
+
+        #endregion
+
+        //----------------------------------------------------------------------------------------------------
+
+        #region Editor Loop
+
+        private void OnValidate()
+        {
+            // remove all other components
+            foreach (Component component in gameObject.GetComponents<Component>())
+            {
+                if ( !(component is Transform || component is HierarchyFolders) )
+                {
+                    Log(component.name + "destroyed: Folders cannot contain other components.", LogType.Warning);
+                    DestroyImmediate(component);
+                }
+            }
+        }
+
+        #endregion
+        
+    } // class end
+}
+        
+#endif
