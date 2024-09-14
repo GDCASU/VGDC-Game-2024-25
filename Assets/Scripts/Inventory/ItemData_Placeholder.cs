5068670b21682c39f17a79b870ca9b3716c3459e
TJ-Elshen <y.e.s@asu.edu>
2024-09-14 13:42:01 -0700

    Created a placeholder class to act as ItemData

---
+++ Assets/Scripts/Inventory/ItemData_Placeholder.cs
@@ -0,0 +1,31 @@
+using Gaskellgames;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+/* -----------------------------------------------------------
+ * Author: TJ (Yousuf)
+ * 
+ * 
+ * Modified By:
+ * 
+ */// --------------------------------------------------------
+
+/* -----------------------------------------------------------
+ * Purpose: A placeholder script to act as the ItemData
+ * 
+ */// --------------------------------------------------------
+
+
+/// <summary>
+/// 
+/// </summary>
+public class ItemData_Placeholder : GGScriptableObject
+{
+    // Use this bool to gate all your Debug.Log Statements please
+    [Header("Debugging")]
+    [SerializeField] private bool doDebugLog;
+
+    public string id;
+    public string displaName;
+}
