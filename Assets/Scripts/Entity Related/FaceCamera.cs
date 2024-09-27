f88b4284999c34ffe1050f63e9ede8df4134b94a
MGlos388 <mtg.shcs@gmail.com>
2024-09-27 14:17:57 -0700

    Edited and implemented the FaceToward script, and added a placeholder input popup for the dungeon entrance function

--- Assets/Scripts/Entity Related/FaceCamera.cs
+++ Assets/Scripts/Entity Related/FaceCamera.cs
@@ -2,6 +2,19 @@ using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
 
+/* -----------------------------------------------------------
+ * Author:
+ * Jacob Kauffman, 
+ * 
+ * Edited:
+ * Matthew Glos
+ */// --------------------------------------------------------
+
+/* -----------------------------------------------------------
+ * Pupose:
+ * Attach to any sprite objects that need to be oriented towards the camera
+ * allows for locking of each axis, and initial offsets
+ */// --------------------------------------------------------
 public class FaceCamera : MonoBehaviour
 {
     [SerializeField] Camera mainCamera;
@@ -12,6 +25,10 @@ public class FaceCamera : MonoBehaviour
     [SerializeField] private bool lockY;
     [SerializeField] private bool lockZ;
 
+    [SerializeField] private float offset_x;
+    [SerializeField] private float offset_y;
+    [SerializeField] private float offset_z;
+
     private Vector3 originalRotation;
 
     private void Awake(){
@@ -20,13 +37,23 @@ public class FaceCamera : MonoBehaviour
 
     void LateUpdate(){
         //Face object toward camera
-        spriteRenderer.transform.LookAt(mainCamera.transform.position, Vector3.up);
+
+        //commented out code here is the old function to orient towards the camera
+        //spriteRenderer.transform.LookAt(mainCamera.transform.position, Vector3.up);
+        spriteRenderer.transform.rotation = mainCamera.transform.rotation;
 
         //Lock object from rotating in specified direction
         Vector3 rotation = spriteRenderer.transform.rotation.eulerAngles;
+
+
         if (lockX) {rotation.x = originalRotation.x;}
         if (lockY) {rotation.x = originalRotation.x;}
-        if (lockZ) {rotation.x = originalRotation.x;} 
+        if (lockZ) {rotation.x = originalRotation.x;}
+
+        rotation.x += offset_x;
+        rotation.y += offset_y;
+        rotation.z += offset_z;
+
         spriteRenderer.transform.rotation = Quaternion.Euler(rotation);
     }
 }
--- Assets/Scripts/Managers/DungeonEntranceManager.cs
+++ Assets/Scripts/Managers/DungeonEntranceManager.cs
@@ -1,8 +1,10 @@
+using System;
 using System.Collections;
 using System.Collections.Generic;
 using Unity.VisualScripting;
 using UnityEngine;
 using UnityEngine.SceneManagement;
+using UnityEngine.SocialPlatforms;
 /* -----------------------------------------------------------
  * Author:
  * Matthew Glos
@@ -15,46 +17,64 @@ using UnityEngine.SceneManagement;
  */// --------------------------------------------------------
 public class DungeonEntranceManager : MonoBehaviour
 {
+    //publicly accessible values for tweaking
     [SerializeField] public GameObject player;
     [SerializeField] public string targetSceneName;
     [SerializeField] public Vector3 targetPosition;
     [SerializeField] public bool isOpen = true;
+    [SerializeField] public float lerpAmount;
+    [SerializeField] SpriteRenderer openSprite, closedSprite, toolTipSprite;
 
+    //private values to keep track of this dungeon entrance's state
+    private bool playerInProximity =false;
     private Scene_Transition_Script sceneManager;
+    
 
     private void Awake()
     {
         sceneManager = GameObject.Find("Scene_Transition_Manager").GetComponent<Scene_Transition_Script>();
         open();
     }
+
+    //Detects when the player is close to the entrance
     private void OnTriggerEnter(Collider other)
     {
-        if (other.gameObject == player && isOpen) {
-            sceneManager.LoadSceneByName(targetSceneName,targetPosition);
-        }
+        if (other.gameObject == player && isOpen) { playerInProximity = true; } 
+    }
+    private void OnTriggerExit(Collider other)
+    {
+        if (other.gameObject == player && isOpen) { playerInProximity = false; }
     }
 
+    //handles visual update of the dungeon entrance
     public void close() {
         isOpen = false;
-        SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
-        sprites[1].enabled = true;
-        sprites[0].enabled = false;
+        closedSprite.enabled = true;
+        openSprite.enabled = false;
     }
 
+    //handles visual update of the dungeon entrance
     public void open() {
         isOpen = true;
-        SpriteRenderer[] sprites = GetComponentsInChildren<SpriteRenderer>();
-        sprites[1].enabled = false;
-        sprites[0].enabled = true;
+        closedSprite.enabled = false;
+        openSprite.enabled = true;
     }
 
     private void Update()
     {
-        if (isOpen){
-            open();
-        } else
+        //actively checks and updates if the publicly accessible bool isOpen changes
+        if (isOpen) { open(); }
+        else { close(); }
+
+
+
+        //basic animation popup for the input tooltip, just lerps between the target scale and current scale
+        toolTipSprite.transform.localScale = new Vector3(Mathf.Lerp(toolTipSprite.transform.localScale.x, Convert.ToInt32(playerInProximity)*2, lerpAmount), 2, 2);
+
+        //loads target scene only when the key E is pressed, the player is in proximity, and the door is open
+        if (Input.GetKeyDown(KeyCode.E) && playerInProximity && isOpen)
         {
-            close();
+            sceneManager.LoadSceneByName(targetSceneName, targetPosition);
         }
     }
 
