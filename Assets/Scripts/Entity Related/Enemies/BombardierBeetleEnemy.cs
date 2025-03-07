b86dcac1b1dd12502eb435d4068ca6caace36a7b
Jose Grijalva <jlgrijal@asu.edu>
2025-03-07 15:15:05 -0700

    New C# Script for [SYS] Bombardier Beetle
    Just got started on creating a new C# Script for the Bombardier Beetle enemy type. No functions implemented yet, but will spend some time looking more into the Google Doc of this task to completely understand what the objectives are for the implementation of this enemy type.

---
+++ Assets/Scripts/Entity Related/Enemies/BombardierBeetleEnemy.cs
@@ -0,0 +1,38 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using System;
+using Pathfinding;
+
+/* -----------------------------------------------------------
+ * Author:
+ * Jose Grijalva
+/* -----------------------------------------------------------
+
+/* -----------------------------------------------------------
+ * Purpose:
+ * Handles the Bombardier Beetle enemy type
+ */// --------------------------------------------------------
+
+/*
+ * TO DO:
+ * - Complete this C# script.
+ * - Add a Capsule Collider 2D to the Bombardier Beetle prefab.
+ * - Add other necessary components to the Bombardier Beetle prefab.
+ */
+
+
+public class BombardierBeetleEnemy : MonoBehaviour
+{
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+}
