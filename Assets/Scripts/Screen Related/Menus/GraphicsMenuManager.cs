e7a85e0dfc5a244baf6373769e164367ce54c9e3
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2025-03-13 18:07:08 -0700

    Brightness Progress
    - Added more code to try to make the brightness slider work but no dice, moving on to more important stuff

--- Assets/Scripts/Screen Related/Menus/GraphicsMenuManager.cs
+++ Assets/Scripts/Screen Related/Menus/GraphicsMenuManager.cs
@@ -3,6 +3,8 @@ using System.Collections.Generic;
 using TMPro;
 using UnityEngine;
 using UnityEngine.UI;
+using UnityEngine.Rendering;
+using UnityEngine.Rendering.Universal;
 
 /* -----------------------------------------------------------
  * Author:
@@ -28,15 +30,39 @@ public class GraphicsMenuManager : MonoBehaviour
     [SerializeField] private Slider brightnessSlider;
     [SerializeField] private TextMeshProUGUI brightnessValueText;
     [SerializeField] private TextMeshProUGUI brightnessShadowText;
+    [SerializeField] private Volume postProcessingVolume;
     
     // Use this bool to gate all your Debug.Log Statements please
     [Header("Debugging")]
     [SerializeField] private bool doDebugLog;
     
+    // Local variables
+    private float brightnessMiddlePoint = 0f; // Middle point will set the postExposure to zero
+    private ColorAdjustments colorAdjustments;
+    
     void Start()
     {
         // Set the fields on load
         fullscreenToggle.isOn = Screen.fullScreen;
+        
+        // Get the middle point of the brightness slider
+        brightnessMiddlePoint = Mathf.Round(brightnessSlider.maxValue / 2f);
+        
+        // FIXME: Unity apparently cant update the profile in real time, not working rn
+        /*
+        // Get Color Adjustments from Post-Processing Volume
+        if (postProcessingVolume.profile.TryGet(out colorAdjustments))
+        {
+            // Load saved brightness or default to middle point
+            float savedBrightness = brightnessMiddlePoint; // TODO: Get brightness stored in data later on
+            brightnessSlider.value = savedBrightness; // Convert range (-10 to 10) → (0 to 20)
+            SetVolumeBrightness(brightnessSlider.value);
+        }
+        else
+        {
+            Debug.LogError("Color Adjustments effect not found in the Post-Processing Volume!");
+        }
+        */
     }
     
     /// <summary>
@@ -56,6 +82,21 @@ public class GraphicsMenuManager : MonoBehaviour
         string valueStr = value.ToString("F0");
         brightnessValueText.text = valueStr;
         brightnessShadowText.text = valueStr;
-        // TODO: Change brightness through post processing
+        SetVolumeBrightness(value);
+    }
+    
+    /// <summary>
+    /// Function to change the brightenss of the screen through the volume post processing effects
+    /// </summary>
+    /// <param name="value"></param>
+    private void SetVolumeBrightness(float value)
+    {
+        // FIXME: Unity apparently cant update the profile in real time, not working rn
+        if (colorAdjustments != null)
+        {
+            float brightnessValue = (value - brightnessMiddlePoint);
+            colorAdjustments.postExposure.value = brightnessValue;
+            // TODO: Save settings
+        }
     }
 }
