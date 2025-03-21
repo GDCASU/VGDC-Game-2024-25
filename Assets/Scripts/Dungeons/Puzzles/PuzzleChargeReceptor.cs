95916d4cbdb15e6726552b059e4591a387a26e26
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2025-03-21 02:05:38 -0700

    Charge Receptor v1
    - Created a charge receptor that triggers with spark and de-activates with water
    - Its still missing its combat behaviours, but it will do for now

--- Assets/Scripts/Dungeons/Puzzles/PuzzleChargeReceptor.cs
+++ Assets/Scripts/Dungeons/Puzzles/PuzzleChargeReceptor.cs
@@ -22,6 +22,8 @@ using UnityEngine.Events;
 /// </summary>
 public class PuzzleChargeReceptor : MonoBehaviour, IDamageable
 {
+    [Header("References")]
+    [SerializeField] private MeshRenderer meshRenderer;
     
     [Header("Events")]
     [SerializeField] private UnityEvent onActivate;
@@ -33,22 +35,38 @@ public class PuzzleChargeReceptor : MonoBehaviour, IDamageable
     [Header("Debugging")]
     [SerializeField] private bool doDebugLog;
     
-    
-    // Start is called before the first frame update
-    void Start()
-    {
-        
-    }
-
-    // Update is called once per frame
-    void Update()
-    {
-        
-    }
+    // Local fields
+    private static readonly int emissionColor = Shader.PropertyToID("_EmissionColor");
 
     public ReactionType TakeDamage(int damage, Elements element)
     {
         // Check if its the spark element
+        if (element == Elements.Sparks && !isActivated)
+        {
+            // It was and it wasnt activated, activate
+            isActivated = true;
+            ChangeReceptorColor(Color.yellow);
+            onActivate?.Invoke();
+            return ReactionType.Undefined;
+        }
+        
+        // Check if its water
+        if (element == Elements.Water && isActivated)
+        {
+            isActivated = false;
+            ChangeReceptorColor(Color.cyan);
+            onDeactivate?.Invoke();
+            return ReactionType.Undefined;
+        }
+        
+        // Else do nothing
         return ReactionType.Undefined;
+    }
+
+    private void ChangeReceptorColor(Color color)
+    {
+        Color finalColor = color * 5f;
+        meshRenderer.material.color = finalColor;
+        meshRenderer.material.SetColor(emissionColor, finalColor);
     }
 }
