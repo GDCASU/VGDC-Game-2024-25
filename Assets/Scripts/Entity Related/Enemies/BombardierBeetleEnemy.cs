a571065c5ced96a9a75ef9253cedcba08e5a899c
Jose Grijalva <jlgrijal@asu.edu>
2025-04-04 16:07:10 -0700

    Declared a few more global variables.
    Declared a few more global variables on ranged attacks. Still trying to figure out what is considered "medium damage" so I can determine the damage value for one of the declared variables.

--- Assets/Scripts/Entity Related/Enemies/BombardierBeetleEnemy.cs
+++ Assets/Scripts/Entity Related/Enemies/BombardierBeetleEnemy.cs
@@ -16,7 +16,7 @@ using Pathfinding;
 
 /*
  * TO DO:
- * - Complete this C# script.
+ * - Complete this C# script using the EntityScript.cs for reference.
  * - Add a Capsule Collider 2D to the Bombardier Beetle prefab.
  * - Add other necessary components to the Bombardier Beetle prefab.
  */
@@ -24,6 +24,19 @@ using Pathfinding;
 
 public class BombardierBeetleEnemy : MonoBehaviour
 {
+    [Header("Control")]
+    [SerializeField] private float _meleeAttackDistance = 4f; // Assuming 2f is 1 zone. 2f & 2 zones = 4f because the attack
+                                                              // range takes up between 2 zones.
+    [SerializeField] private float _rangedAttackCooldown = 3f; // Google Doc does not specify the cooldown time for the ranged attack
+                                                               // so it is assumed that it is 3f like with the Big Mushroom Guy's cooldown.
+    [SerializeField] private float _rangedAttackRadius;
+
+
+
+    private AIPath _aiPath;
+
+
+
     // Start is called before the first frame update
     void Start()
     {
