6a7b4ef19addd11484a0451e4736f21de2f9d148
FalconKiaT <117485567+FalconKiaT@users.noreply.github.com>
2025-05-02 10:22:15 -0700

    Level 2 Working
    - Level 2 has combat and room data, only missing the NPCs and Lore notes

--- Assets/Scripts/Combat/DamageDetector.cs
+++ Assets/Scripts/Combat/DamageDetector.cs
@@ -6,7 +6,7 @@ using UnityEngine.Events;
 public class DamageDetector : MonoBehaviour, IDamageable
 {
     public UnityEvent<int, Elements> onDamageTaken = new();
-    public ReactionType TakeDamage(int damage, Elements element)
+    public ReactionType TakeDamage(int damage, Elements element, Vector3 direction)
     {
         onDamageTaken?.Invoke(damage, element);
 
